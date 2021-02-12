4bd9db98fa3336197a55ba4c29fe0854cebb5636
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-12 11:34:58 +0600

    style home component and Quzie component on card text section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import Quize from './Quize'
 
 const Home = () => {
    return (
       <div>
+         <div className="container">
+            <div className="row">
+               <div className="col-md-12">
+                  <div className="card bg-dark col-lg-12" >
+                     <div className="card-body">
+                        <h2 className="card-title">Hellow world</h2>
+                        {/* <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6> */}
+                        <p className="card-text">
+                           <Quize></Quize>
+                        </p>
 
+                     </div>
+                  </div>
+
+               </div>
+            </div>
+         </div>
       </div>
    )
 }
