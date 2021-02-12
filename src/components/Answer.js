41fd628ae63c2f726cf43527c87c2f08dd037a37
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-12 12:08:20 +0600

    set logic for randomize our answer

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
-const Answer = () => {
+const Answer = ({ answer }) => {
    return (
-      <div>
-         <label htmlFor="" className='btn btn-lg btn-secondary mt-2 mr-1 w-100'>
-            <input type="radio" name='option' id='1' className='' />
-            Answer
+      <Fragment>
+         <label htmlFor="1" className='btn btn-lg btn-secondary mt-2 mr-1 '>
+            <input type="radio" name='option' id='1' className='mr-3' />
+            {answer}
          </label>
-      </div>
+      </Fragment>
    )
 }
 
--- src/components/Quize.js
+++ src/components/Quize.js
@@ -3,13 +3,15 @@ import Answer from './Answer'
 import Question from './Question'
 
 const Quize = () => {
+   const ansers = [2, 4, 5, 7]
+   const result = ansers.map(answer => <Answer answer={answer} />)
    return (
       <div>
          <div className="card-body">
             <Question />
             <div className="card-text">
-               <div className="btn-group btn-group-vertical toggle w-100" data-toggle='button'>
-                  <Answer />
+               <div className="btn-group btn-group-vertical toggle " data-toggle='button'>
+                  {result.sort(() => Math.random() - 0.5)}
                </div>
             </div>
          </div>
