b4ff5b19f286ba3215f7f59a9f8027523b7e6825
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-12 11:19:01 +0600

    install firebase and add config fire for firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+   apiKey: "AIzaSyDFUMmXzW8UztkI7x_NSgG74wVo-ZOjKKM",
+   authDomain: "react-quize-app.firebaseapp.com",
+   projectId: "react-quize-app",
+   storageBucket: "react-quize-app.appspot.com",
+   messagingSenderId: "166070341463",
+   appId: "1:166070341463:web:24132c4cedac1d9e617812",
+   measurementId: "G-X55XY22G3T"
+};
